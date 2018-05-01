7215656a24f68013d9b8beadde58852f6f94b369
CaioBabler <caio.babler@gmail.com>
2018-05-01 14:14:46 -0300

    nivel de acesso

---
+++ code/DomGo/Domgo-core/src/main/java/com/unity/domgo/model/NivelDeAcesso.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unity.domgo.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author user
+ */
+public enum NivelDeAcesso {
+    ROOT, ADMIN, USER;
+
+}
