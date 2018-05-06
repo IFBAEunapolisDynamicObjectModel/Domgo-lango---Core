152594594426308d2016384ec7271c64925a70f8
CaioBabler <caio.babler@gmail.com>
2018-05-05 21:47:00 -0300

    .

---
+++ Domgo-Core/src/main/java/com/unity/domgocore/model/Atributo.java
@@ -0,0 +1,101 @@
+package com.unity.domgocore.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
+import com.unity.domgocore.model.RegraAtributo;
+import java.util.Set;
+import java.util.HashSet;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Atributo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@ManyToMany(cascade = CascadeType.ALL)
+	private Set<RegraAtributo> regra = new HashSet<RegraAtributo>();
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Atributo)) {
+			return false;
+		}
+		Atributo other = (Atributo) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (nome != null && !nome.trim().isEmpty()) {
+			result += "nome: " + nome;
+		}
+		return result;
+	}
+
+	public Set<RegraAtributo> getRegra() {
+		return this.regra;
+	}
+
+	public void setRegra(final Set<RegraAtributo> regra) {
+		this.regra = regra;
+	}
+
+}
---
+++ Domgo-Core/src/main/java/com/unity/domgocore/model/RegraAtributo.java
@@ -0,0 +1,95 @@
+package com.unity.domgocore.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+
+@Entity
+public class RegraAtributo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column
+	private String descricao;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof RegraAtributo)) {
+			return false;
+		}
+		RegraAtributo other = (RegraAtributo) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (nome != null && !nome.trim().isEmpty())
+			result += "nome: " + nome;
+		if (descricao != null && !descricao.trim().isEmpty())
+			result += ", descricao: " + descricao;
+		return result;
+	}
+}
